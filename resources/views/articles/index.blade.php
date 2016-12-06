3a5a5db4fb812e435a13d13db38c79a90a18d691
making <wuyanping1029@foxmail.com>
2016-12-06 20:37:22 +0800

    model

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,6 +3,7 @@
 <h1>Article</h1>
 @foreach($article as $key => $values)
 <ul>
+	<li>{{$values->updated_at}}</li>
 	<li>{{$values->title}}</li>
 	<li>{{$values->content}}</li>
 	<li>{{$values->published_at}}</li>
