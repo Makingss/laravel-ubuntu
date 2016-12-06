415a5cc8f7301dfcb8c7288eeb1ca58e762ed7f4
making <wuyanping1029@foxmail.com>
2016-12-06 15:53:24 +0800

    article Model

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,19 +4,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Carbon;
+use App\Article;
 
 class ArticleController extends Controller
 {
 
     public function index()
     {
-        $article = new App\Article;
-        $articleData = $article::all();
+        $article=Article::all();
         // $article->title = '';
         // $article->content = '';
         // $article->published_at = Carbon\Carbon::now();
         // return view('article',compact('article'));
-        
-        return ($articleData);
+        return $article;
     }
 }
