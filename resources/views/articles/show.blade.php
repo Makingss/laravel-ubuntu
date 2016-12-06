6331d324159d579a5cc9ee7c7ebe6b3c05654002
making <wuyanping1029@foxmail.com>
2016-12-06 22:33:13 +0800

    show

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,8 +1,8 @@
 @extends('app') @section('content')
 
-<h1>{{$article->title}}</h1>
-	<article>
-	<div class="body"></div>
-	{{$values->content}}
-	</article>
+    <h1>{{$article->title}}</h1>
+    <article>
+        <div class="body"></div>
+        {{$article->content}}
+    </article>
 @stop
