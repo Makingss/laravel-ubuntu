d234fdc3609dd7fa4828547238984192a8b20826
making <wuyanping1029@foxmail.com>
2016-12-06 17:58:49 +0800

    index model

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,9 +1,10 @@
 @extends('app') @section('content')
 
 <h1>Article</h1>
-@if(count($article)>0)
-<ul>
 	@foreach($article as $key => $values)
-	<li>{{$values}}</li> @endforeach
-</ul>
-@entif @stop
+	<ul>
+	<li>{{$values}}</li> 
+	</ul>
+	@endforeach
+
+@stop
