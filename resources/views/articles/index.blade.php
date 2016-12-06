e80dea029a5cfa08367f1aec3bda2f09c6a2cbfb
making <wuyanping1029@foxmail.com>
2016-12-06 18:49:44 +0800

    blade model

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,16 +1,10 @@
 @extends('app') @section('content')
 
 <h1>Article</h1>
-	@foreach($article as $key => $values)
-	<ul>
-	<li>{{$values->title}}</li> 
-	</ul>
-	<ul>
-	<li>{{$values->content}}</li> 
-	</ul>
-	<ul>
-	<li>{{$values->published_at}}</li> 
-	</ul>
-	@endforeach
-
-@stop
+@foreach($article as $key => $values)
+<ul>
+	<li>{{$values->title}}</li>
+	<li>{{$values->content}}</li>
+	<li>{{$values->published_at}}</li>
+</ul>
+@endforeach @stop
