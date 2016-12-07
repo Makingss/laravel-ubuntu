53ac6596b7bfb32232adea67c04918e0b48d0af4
making <wuyanping1029@foxmail.com>
2016-12-07 17:51:29 +0800

    html

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,4 +1,10 @@
 @extends('app') @section('content')
     <h1>Create Article</h1>
-                    form()
+    <form action="">
+        <dev class="body">
+            <textarea name="textarea" id="" cols="30" rows="10">
+                Dfdf
+            </textarea>
+        </dev>
+    </form>
 @stop
