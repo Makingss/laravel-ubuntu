7dfa765b3562705fa520622c174c61f11b9775cc
making <wuyanping1029@foxmail.com>
2016-12-07 19:32:54 +0800

    create.html

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,10 +1,10 @@
 @extends('app') @section('content')
     <h1>Create Article</h1>
     {!! Form::open() !!}
-    {!! Form::lable('name') !!}
+    {!! Form::label('name') !!}
     {!! Form::text('name') !!}
     {!! Form::close() !!}
-    <!--<form action="">
+            <!--<form action="">
         <dev class="body">
             <textarea name="textarea" id="" cols="30" rows="10">
                 Dfdf
