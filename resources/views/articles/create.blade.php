7abdc68f6d4d0afcf0dc1a99b4d1217fd21b9081
making <wuyanping1029@foxmail.com>
2016-12-07 22:30:23 +0800

    create.blade

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,9 +1,13 @@
 @extends('app') @section('content')
     <h1>撰写新文章</h1>
     {!! Form::open() !!}
-            <div class="from-groups">
+            <div class="form-group">
     {!! Form::label('Title') !!}
     {!! Form::text('Title',null,['class'=>'form-control']) !!}
             </div>
+    <div class="form-group">
+        {!! Form::label('content','Content:') !!}
+        {!! Form::textarea('content',null,'form-controls') !!}
+    </div>
     {!! Form::close() !!}
 @stop
