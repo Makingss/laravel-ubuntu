c6b0115b56b9c2d475753114e898d1a7e3d89c93
root <root@bogon.localdomain>
2016-12-07 07:48:45 -0800

    Articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,7 +10,7 @@ class ArticleController extends Controller
 {
     public function index()
     {
-        $article = Article::all();
+        $article = Article::latest()->get();
         return view('articles.index', compact('article'));
     }
 
