378baf4549873e37945c0cb2374790513b21ca56
making <wuyanping1029@foxmail.com>
2016-12-07 18:00:07 +0800

    lkk

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,10 +1,14 @@
 @extends('app') @section('content')
     <h1>Create Article</h1>
-    <form action="">
+    {!! Form::open() !!}}
+
+    {!! Form::close() !!}}
+    <!--<form action="">
         <dev class="body">
             <textarea name="textarea" id="" cols="30" rows="10">
                 Dfdf
             </textarea>
         </dev>
     </form>
+    -->
 @stop
