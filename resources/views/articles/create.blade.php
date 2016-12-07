994120ef75c9cb0516c23bcf80bdd83970191519
making <wuyanping1029@foxmail.com>
2016-12-07 23:24:22 +0800

    dd

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,6 +1,6 @@
 @extends('app') @section('content')
     <h1>撰写新文章</h1>
-    {!! Form::open([url=>'/articles']) !!}
+    {!! Form::open(['url'=>'/articles']) !!}
     <div class="form-groups">
         {!! Form::label('Title') !!}
         {!! Form::text('Title',null,['class'=>'form-control']) !!}
