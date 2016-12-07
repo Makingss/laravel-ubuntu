64fce4ec9bcda8ffbd4b2271a13233f4df25ff5c
making <wuyanping1029@foxmail.com>
2016-12-07 22:22:31 +0800

    create.blade

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,8 +2,8 @@
     <h1>撰写新文章</h1>
     {!! Form::open() !!}
             <div class="from-groups">
-    {!! Form::label('name') !!}
-    {!! Form::text('name',null,['class'=>'from-control']) !!}
+    {!! Form::label('Title') !!}
+    {!! Form::text('Title',null,['class'=>'form-control']) !!}
             </div>
     {!! Form::close() !!}
 @stop
