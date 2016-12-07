c0592601b92ede27c97681ac647dc54ed613b9d4
root <root@bogon.localdomain>
2016-12-07 06:24:35 -0800

    create.blade

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,7 +1,7 @@
 @extends('app') @section('content')
     <h1>撰写新文章</h1>
     {!! Form::open() !!}
-            <div class="from-groups">
+            <div class="form-groups">
     {!! Form::label('Title') !!}
     {!! Form::text('Title',null,['class'=>'form-control']) !!}
             </div>
