f56a9475c88f8bec516989b1adbc7e26055ac7ef
making <wuyanping1029@foxmail.com>
2016-12-08 20:51:15 +0800

    create.blade

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -15,11 +15,11 @@
     </div>
     {!! Form::submit('发表文章',['class'=>'btn btn-primary form-control']) !!}
     {!! Form::close() !!}
-	@if($errors->any())
-        <ul class="alert alert-danger">
+    @if($errors->any())
+        <ul class="alert alert-warning">
             @foreach($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
         </ul>
-@endif
+    @endif
 @stop
