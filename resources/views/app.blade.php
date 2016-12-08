00fbcf1c7036872ad03331b8801994fb757b1c1e
making <wuyanping1029@foxmail.com>
2016-12-08 08:49:36 +0800

    app.blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,18 @@
 <html>
 	<head>
 		 <title>Making.com</title>
-		 <link href="//cdn.bootcss.com/bootstrap/4.0.0-alpha.5/css/bootstrap-flex.min.css">
+		<!-- 新 Bootstrap 核心 CSS 文件 -->
+		<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap.min.css">
+
+		<!-- 可选的Bootstrap主题文件（一般不用引入） -->
+		<link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
+
+		<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+		<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+		<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+		<script src="http://cdn.bootcss.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+
 	</head>
 	<body>
 		@yield('haed')
