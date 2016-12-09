a3656e00113e1ccd7fe1d838e976fb3b18bc1021
making <wuyanping1029@foxmail.com>
2016-12-09 18:24:10 +0800

    welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-    <div class="flex-center position-ref full-height">
+    <!--<div class="flex-center position-ref full-height">
         @if (Route::has('login'))
             <div class="top-right links">
                 @if (Auth::check())
@@ -10,9 +10,26 @@
                     <a href="{{ url('/register') }}">注册</a>
                 @endif
             </div>
+        -->
         @endif
         <div class="row">
             <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img data-src="holder.js/300x300" alt="...">
+                    <div class="caption">
+                        <h3>Thumbnail label</h3>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                </div>
+                <div class="thumbnail">
+                    <img data-src="holder.js/300x300" alt="...">
+                    <div class="caption">
+                        <h3>Thumbnail label</h3>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                </div>
                 <div class="thumbnail">
                     <img data-src="holder.js/300x300" alt="...">
                     <div class="caption">
