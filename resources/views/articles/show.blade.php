e4351ea23d13f72b99ac30bd436d9eed38a0c74e
making <wuyanping1029@foxmail.com>
2016-12-09 13:41:29 +0800

    show.blade

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -5,7 +5,7 @@
         <div class="body"></div>
         {{$article->content}}
     </article>
-    <button type="button" class="btn btn-default btn-link">
+    <button type="button" class="btn btn-default btn-lg">
         <span class="glyphicon glyphicon-pencil"></span>修改文章
     </button>
 @stop
