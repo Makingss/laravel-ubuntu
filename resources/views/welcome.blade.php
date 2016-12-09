ce2bed473a03f8a52c246f7fad41dc46ac663c1b
making <wuyanping1029@foxmail.com>
2016-12-09 18:12:55 +0800

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,85 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <!-- <style>
+         html, body {
+             background-color: #fff;
+             color: #636b6f;
+             font-family: 'Raleway', sans-serif;
+             font-weight: 100;
+             height: 100vh;
+             margin: 0;
+         }
 
-            .full-height {
-                height: 100vh;
-            }
+         .full-height {
+             height: 100vh;
+         }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+         .flex-center {
+             align-items: center;
+             display: flex;
+             justify-content: center;
+         }
 
-            .position-ref {
-                position: relative;
-            }
+         .position-ref {
+             position: relative;
+         }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+         .top-right {
+             position: absolute;
+             right: 10px;
+             top: 18px;
+         }
 
-            .content {
-                text-align: center;
-            }
+         .content {
+             text-align: center;
+         }
 
-            .title {
-                font-size: 84px;
-            }
+         .title {
+             font-size: 84px;
+         }
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+         .links > a {
+             color: #636b6f;
+             padding: 0 25px;
+             font-size: 12px;
+             font-weight: 600;
+             letter-spacing: .1rem;
+             text-decoration: none;
+             text-transform: uppercase;
+         }
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">登录</a>
-                        <a href="{{ url('/register') }}">注册</a>
-                    @endif
-                </div>
+         .m-b-md {
+             margin-bottom: 30px;
+         }
+     </style>
+     -->
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @if (Auth::check())
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ url('/login') }}">登录</a>
+                <a href="{{ url('/register') }}">注册</a>
             @endif
-
+        </div>
+        @endif
+                <!--
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MAKING
                 </div>
 
                 <div class="links">
@@ -90,6 +91,19 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+            -->
+        <div class="row">
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img data-src="holder.js/300x300" alt="...">
+                    <div class="caption">
+                        <h3>Thumbnail label</h3>
+                        <p>...</p>
+                        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+                    </div>
+                </div>
+            </div>
         </div>
-    </body>
+</div>
+</body>
 </html>
