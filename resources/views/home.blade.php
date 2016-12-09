babab9abb5bd5e4f66988a7b3d697c90279ff115
making <wuyanping1029@foxmail.com>
2016-12-09 11:46:41 +0800

    login.index

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">等着吧！</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    首面还没有完善！
                 </div>
             </div>
         </div>
