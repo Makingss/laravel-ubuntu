7c61a35592e05cdfc1cbf76b46d1e5dd37b9b549
making <wuyanping1029@foxmail.com>
2016-12-09 17:53:56 +0800

    app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,7 +50,9 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @if (Auth::guest())
+                            <button class="btn btn-primary">
                             <li><a href="{{ url('/login') }}">登录</a></li>
+                            </button>
                             <li><a href="{{ url('/register') }}">加入MAKING</a></li>
                         @else
                             <li class="dropdown">
