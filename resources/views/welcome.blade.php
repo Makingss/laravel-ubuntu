8bfdba67ed33ea781f764730d0454d10b755c853
making <wuyanping1029@foxmail.com>
2016-12-11 10:04:44 +0800

    welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@
             <img data-src="holder.js/300x300" alt="...">
             <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
             </div>
         </div>
@@ -28,7 +28,7 @@
             <img data-src="holder.js/300x300" alt="...">
             <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
             </div>
         </div>
@@ -38,7 +38,7 @@
             <img data-src="holder.js/300x300" alt="...">
             <div class="caption">
                 <h3>Thumbnail label</h3>
-                <p>...</p>
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
             </div>
         </div>
