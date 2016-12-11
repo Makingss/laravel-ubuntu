cb449a7ebba5169eef726e2a5e475d55edb0750b
making <wuyanping1029@foxmail.com>
2016-12-11 10:00:11 +0800

    welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,8 +23,16 @@
             </div>
         </div>
     </div>
-</div>
-<div class="row">
+    <div class="col-sm-6 col-md-4">
+        <div class="thumbnail">
+            <img data-src="holder.js/300x300" alt="...">
+            <div class="caption">
+                <h3>Thumbnail label</h3>
+                <p>...</p>
+                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            </div>
+        </div>
+    </div>
     <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
             <img data-src="holder.js/300x300" alt="...">
@@ -36,17 +44,6 @@
         </div>
     </div>
 </div>
-<div class="row">
-    <div class="col-sm-6 col-md-4">
-        <div class="thumbnail">
-            <img data-src="holder.js/300x300" alt="...">
-            <div class="caption">
-                <h3>Thumbnail label</h3>
-                <p>...</p>
-                <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 @stop
