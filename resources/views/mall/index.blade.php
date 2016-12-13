453ac9b863601badc13ae523c750014a6760dc3d
root <root@bogon.localdomain>
2016-12-13 03:10:07 -0800

    test

--- resources/views/mall/index.blade.php
+++ resources/views/mall/index.blade.php
@@ -3,42 +3,7 @@
 <!--[if IE 8]><html class="ie ie8 lte9 lte8" lang="en-US">	<![endif]-->
 <!--[if IE 9]><html class="ie ie9 lte9" lang="en-US"><![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--><html class="noIE" lang="en-US"><!--<![endif]-->
-<head>
-	<meta charset="UTF-8" />
-	<title>GFashion - Responsive e-commerce HTML Template</title>
-	<meta name="description" content=""/>
-	<meta name="keywords" content=""/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	
-	<!-- Favorite Icons -->
-	<link rel="icon" href="img/favicon/favicon.html" type="image/x-icon" />
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144-precomposed.html">
-	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72-precomposed.html">
-	<link rel="apple-touch-icon-precomposed" href="img/favicon/apple-touch-icon-precomposed.html">
-	<!-- // Favorite Icons -->
-	
-	<link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
-	
-	<!-- GENERAL CSS FILES -->
-	<link rel="stylesheet" href="css/minified.css">
-	<!-- // GENERAL CSS FILES -->
-	
-	<!--[if IE 8]>
-		<script src="js/respond.min.js"></script>
-		<script src="js/selectivizr-min.js"></script>
-	<![endif]-->
-	<!--
-	<script src="js/jquery.min.js"></script>
-	-->
-	<script>window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>');</script>
-	<script src="js/modernizr.min.js"></script>	
-	<!-- PARTICULAR PAGES CSS FILES -->
-	<link rel="stylesheet" href="css/owl.carousel.css">
-	<link rel="stylesheet" href="css/owl.theme.css">
-	<link href="css/flexslider.css" rel="stylesheet" />
-	<!-- // PARTICULAR PAGES CSS FILES -->
-	<link rel="stylesheet" href="css/responsive.css">
-</head>
+@extends('layouts.app')
 <body class="home">
 			
 	<!-- PAGE WRAPPER -->
