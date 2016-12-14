5825b314ca09757b450b4ad2806457893b5ebf55
making <wuyanping1029@foxmail.com>
2016-12-14 11:00:44 +0800

    layouts

---
+++ resources/views/mall/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<!--[if IE 7 ]><html class="ie ie7 lte9 lte8 lte7" lang="en-US"><![endif]-->
+<!--[if IE 8]><html class="ie ie8 lte9 lte8" lang="en-US">	<![endif]-->
+<!--[if IE 9]><html class="ie ie9 lte9" lang="en-US"><![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html class="noIE" lang="en-US"><!--<![endif]-->
+
+@include('dead')
+@include('body')
+@include('main')
+@include('footer')
+</html>
