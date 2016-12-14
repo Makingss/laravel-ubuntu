deac08561b4a8a7bcb5572696cc9b2a47067c8ae
making <wuyanping1029@foxmail.com>
2016-12-14 14:53:58 +0800

    app.blade

--- resources/views/mall/layouts/app.blade.php
+++ resources/views/mall/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
 <head>
 	<meta charset="UTF-8" />
-	<title>GFashion - Responsive e-commerce HTML Template</title>
+	<title>官方商城</title>
 	<meta name="description" content=""/>
 	<meta name="keywords" content=""/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
