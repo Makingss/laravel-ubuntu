865305d336c051a8ab17f0a1fd87f0db426c938d
making <wuyanping1029@foxmail.com>
2016-12-16 20:07:07 +0800

    Notifications

--- app/Notifications/UserMailPublished.php
+++ app/Notifications/UserMailPublished.php
@@ -2,6 +2,7 @@
 
 namespace App\Notifications;
 
+use App\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -10,54 +11,54 @@ use Illuminate\Notifications\Messages\MailMessage;
 class UserMailPublished extends Notification
 {
     use Queueable;
+    public $user;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
-    }
+        $this->user = $user;
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
+        /**
+         * Get the notification's delivery channels.
+         *
+         * @param  mixed $notifiable
+         * @return array
+         */
+        public function via($notifiable)
+        {
+            return ['mail'];
+        }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
-    public function toMail($notifiable)
-    {
-        return (new MailMessage)
-                    ->success()
-                    ->greeting('Dear Making')
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', 'https://laravel.com')
-                    ->line('Thank you for using our application!');
-    }
+        /**
+         * Get the mail representation of the notification.
+         *
+         * @param  mixed $notifiable
+         * @return \Illuminate\Notifications\Messages\MailMessage
+         */
+        public function toMail($notifiable)
+        {
+            return (new MailMessage)
+                ->success()
+                ->greeting('Dear' . $notifiable->user->name)
+                ->line('The introduction to the notification.')
+                ->action('Notification Action', 'https://laravel.com')
+                ->line('Thank you for using our application!');
+        }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        /**
+         * Get the array representation of the notification.
+         *
+         * @param  mixed $notifiable
+         * @return array
+         */
+        public function toArray($notifiable)
+        {
+            return [
+                //
+            ];
+        }
     }
-}
