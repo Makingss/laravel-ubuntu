ee421a55bb7494f0b0d56278e8724ad4daab2322
root <root@bogon.localdomain>
2016-12-16 21:49:24 -0800

    Home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,16 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+		<passport-clients></passport-clients>
+<passport-authorized-clients></passport-authorized-clients>
+<passport-personal-access-tokens></passport-personal-access-tokens>
+           <!-- <div class="panel panel-default">
                 <div class="panel-heading">等着吧！</div>
 
                 <div class="panel-body">
                     首面还没有完善！
                 </div>
-            </div>
+            </div>-->
         </div>
     </div>
 </div>
