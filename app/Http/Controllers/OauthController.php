463ac84b0f3b6be039e6ef0b3ad7595b3a87190e
making <wuyanping1029@foxmail.com>
2016-12-22 19:33:26 +0800

    OauthController

---
+++ app/Http/Controllers/OauthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OauthController extends Controller
+{
+    public function oauth(Request $request)
+    {
+        $http = new GuzzleHttp\Client;
+        $response = $http->post('http://your-app.com/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'authorization_code',
+                'client_id' => '3',
+                'client_secret' => 'W8IuK4Ighp56qNGnweMiY0NsmYswhSusy4L1aEF9',
+                'redirect_uri' => 'http://passport-client.dev/callback',
+                'code' => $request->code,
+            ],
+        ]);
+
+        return json_decode((string)$response->getBody(), true);
+    }
+}
+
