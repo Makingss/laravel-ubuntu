be161de7febe3bf5c53ee917ff9843467818bf59
making <wuyanping1029@foxmail.com>
2016-12-22 19:04:31 +0800

    AuthSserviceProvide

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,20 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        /**
+         * Auth routes
+         */
+        Passport::routes();
+        /**
+         *
+         * The date when access tokens expire.
+         *   访问令牌过期的日期
+         */
+        Passport::tokensExpireIn(Carbon::now()->addDays(15));
+        /**
+         * The date when refresh tokens expire.
+         * 刷新令牌过期的日期
+         */
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
     }
 }
