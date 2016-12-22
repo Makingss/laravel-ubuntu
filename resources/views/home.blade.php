6b03cd564550cf85843ead69000e6e11bb7ed7c5
making <wuyanping1029@foxmail.com>
2016-12-22 19:08:40 +0800

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-		<passport-clients></passport-clients>
-<passport-authorized-clients></passport-authorized-clients>
-<passport-personal-access-tokens></passport-personal-access-tokens>
-           <div class="panel panel-default">
-                <div class="panel-heading">等着吧！</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <passport-clients></passport-clients>
+                <passport-authorized-clients></passport-authorized-clients>
+                <passport-personal-access-tokens></passport-personal-access-tokens>
+                <div class="panel panel-default">
+                    <div class="panel-heading">等着吧！</div>
 
-                <div class="panel-body">
-                    首面还没有完善！
+                    <div class="panel-body">
+                        首面还没有完善！
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
