bd0659b07ca2bc97b426555d0645615f3b4fc71d
making <wuyanping1029@foxmail.com>
2016-12-25 14:42:17 +0800

    notification

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -10,17 +10,17 @@ use Illuminate\Support\Facades\Redirect;
 
 class Notification extends Controller
 {
-//    public function __construct()
-//    {
-//
-//    }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
     public function showNotitfcation()
     {
-        //如果用户没有登录,重定向到login,成功登录后返回当前页面
-        if(Auth::check() == false){
-            return Redirect::guest('login');
-        }
-         Auth::user()->notify(new UserSubScribe());
+//        //如果用户没有登录,重定向到login,成功登录后返回当前页面
+//        if(Auth::check() == false){
+//            return Redirect::guest('login');
+//        }
+        Auth::user()->notify(new UserSubScribe());
     }
 }
