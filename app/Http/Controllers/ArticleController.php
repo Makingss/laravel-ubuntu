1b15c793809896bf7b2066c6d20bb0a542c148c6
making <wuyanping1029@foxmail.com>
2016-12-25 14:39:12 +0800

    ArticleController

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,9 +17,7 @@ class ArticleController extends Controller
     public function __construct()
     {
         //如果用户没有登录,重定向到login,成功登录后返回当前页面
-        if(Auth::check() == false){
-            return Redirect::guest('login');
-        }
+        $this->middleware('auth');
     }
 
     public function index()
