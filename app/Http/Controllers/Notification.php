8f68670b689c8db90607cd5cc68b2f35ff8b7269
making <wuyanping1029@foxmail.com>
2016-12-25 11:56:18 +0800

    Notification

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -5,19 +5,18 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Notifications\UserSubScribe;
 class Notification extends Controller
 {
-    protected $user;
-
-    public function __construct(Auth $user)
-    {
-        $this->user = $user;
-    }
+//    protected $user;
+//
+//    public function __construct(Auth $user)
+//    {
+//        $this->user = $user;
+//    }
 
     public function showNotitfcation()
     {
-        dd($this->user);
-        $this->user->notify(new \App\Notifications\UserSubScribe());
+        Auth::user()->notify(new UserSubScribe());
     }
 }
