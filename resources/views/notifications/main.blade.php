5dcc29b261a37df1b3e938dfac3b0579cb5da10b
making <wuyanping1029@foxmail.com>
2016-12-25 16:14:06 +0800

    main

--- resources/views/notifications/main.blade.php
+++ resources/views/notifications/main.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
+
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">我的通知
-                        
-                    </div>
+                    <div class="panel-heading">我的通知</div>
 
                     <div class="panel-body">
                         首面还没有完善！
@@ -16,4 +15,3 @@
         </div>
     </div>
 @endsection
-@stop
