079d71cb2a9979c25ae1019d581e9d42c4ef89fd
making <wuyanping1029@foxmail.com>
2016-12-25 14:24:21 +0800

    Notification

--- app/Http/Controllers/Notification.php
+++ app/Http/Controllers/Notification.php
@@ -6,6 +6,8 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Notifications\UserSubScribe;
+use Illuminate\Support\Facades\Redirect;
+
 class Notification extends Controller
 {
     public function __construct()
