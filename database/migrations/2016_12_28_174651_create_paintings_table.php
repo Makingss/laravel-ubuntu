94b645a30841587e0f821c68685b3d64008a44cd
making <wuyanping1029@foxmail.com>
2016-12-28 18:00:25 +0800

    migartion Model

---
+++ database/migrations/2016_12_28_174651_create_paintings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePaintingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('paintings', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->integer('painter_id');
+            $table->string('title');
+            $table->text('body');
+            $table->timestamp('completed_at');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('paintings');
+    }
+}
