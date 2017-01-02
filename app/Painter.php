838b757dfe520e28805a92524b3260c6ca564818
making <wuyanping1029@foxmail.com>
2017-01-02 11:29:37 +0800

    model painter

--- app/Painter.php
+++ app/Painter.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Painter extends Model
 {
+    protected $fillable = ['username', 'bio'];
+
     public function Paintings()
     {
         return $this->hasMany(Painting::class, 'painter_id');
