0600eb59b24f025e9581d39ece72a472f2b3b5f0
making <wuyanping1029@foxmail.com>
2017-01-05 10:43:26 +0800

    migrations Painting

--- database/migrations/2016_12_28_174651_create_paintings_table.php
+++ database/migrations/2016_12_28_174651_create_paintings_table.php
@@ -15,11 +15,11 @@ class CreatePaintingsTable extends Migration
     {
         Schema::create('paintings', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-            $table->increments('id')->comment('ID');
-            $table->integer('painter_id')->comment('painter_id');
-            $table->string('title')->comment('标题');
-            $table->text('body')->comment('内容');
-            $table->timestamp('completed_at')->comment('发布时间');
+            $table->increments('id')->comment('ID')->change();
+            $table->integer('painter_id')->comment('painter_id')->change();
+            $table->string('title')->comment('标题')->change();
+            $table->text('body')->comment('内容')->change();
+            $table->timestamp('completed_at')->comment('发布时间')->change();
             #$table->timestamp('created_at');
             #$table->timestamp('updated_at');
             $table->timestamps();
