5dd031142b49692c1bfe1c879614c030c5be2325
making <wuyanping1029@foxmail.com>
2017-01-05 14:50:35 +0800

    paintings

--- app/Admin/Controllers/PainterController.php
+++ app/Admin/Controllers/PainterController.php
@@ -49,9 +49,10 @@ class PainterController extends Controller
             $grid->paintings('名称')->pluck('title')->map(function ($title) {
                 return "<strong><i>《" . $title . "》</i></strong>";
             })->implode('<br/>');
-            $grid->director()->value(function ($id) {
-                return Painting::find($id)->body;
-            });
+            $grid->paintings()->body();
+//            $grid->director()->value(function ($id) {
+//                return Painting::find($id)->body;
+//            });
 //            $grid->paintings('内容')->pluck('body')->map(function ($body) {
 //                return $body;
 //            })->implode('body');
