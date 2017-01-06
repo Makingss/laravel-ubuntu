7bc72462c764337c9c1386bdeadd8df0d041932c
making <wuyanping1029@foxmail.com>
2017-01-06 16:52:12 +0800

    config/dbschema

--- config/dbschema.php
+++ config/dbschema.php
@@ -4,4 +4,48 @@
  * User: Making
  * Date: 2017/1/6
  * Time: 14:24
+ * dbschema tables
  */
+return[
+    'databases'=>[
+        /**
+         * 商品主表
+         */
+        'goods_tables'=>'goods',
+        /**
+         * 货品主表
+         */
+        'products_tables'=>'products',
+        /**
+         * 品牌表
+         */
+        'brand_tables'=>'brands',
+        /**
+         * 商品类型表
+         */
+        'goods_type_tables'=>'goods_type',
+        /**
+         * 类别属性表
+         */
+        'goods_cat_tables'=>'goods_cat',
+        /**
+         * 图片表
+         */
+        'images_tables'=>'images',
+        /**
+         * 库位表
+         */
+        'goods_tip_tables'=>'goods_tip',
+        /**
+         * 商品单位表
+         */
+        'goods_unit_tables'=>'goods_unit',
+        /**
+         * 商品会员等级价格
+         */
+        'goods_lv_price_tables'=>'goods_lv_price',
+        /**
+         *
+         */
+    ],
+];
