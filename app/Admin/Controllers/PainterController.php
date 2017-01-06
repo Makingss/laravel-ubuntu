7f720697b9d0bc601f12a8db1e8adb40bdfe5550
making <wuyanping1029@foxmail.com>
2017-01-06 09:28:16 +0800

    Painting

--- app/Admin/Controllers/PainterController.php
+++ app/Admin/Controllers/PainterController.php
@@ -38,7 +38,7 @@ class PainterController extends Controller
     protected function grid()
     {
         $grids = \Encore\Admin\Facades\Admin::grid(Painter::class, function (Grid $grid) {
-            Painting::latest()->completed()->get();
+            #Painting::latest()->completed()->get();
             #dd($grid);
             $grid->filter(function ($filter) {
                 $filter->is('username', 'username');
