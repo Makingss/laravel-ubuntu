8bfd526dbee3b2e954f418328a1ad1998e7e642c
making <wuyanping1029@foxmail.com>
2017-01-06 23:33:27 +0800

    GoodsController

--- app/Admin/Controllers/GoodsController.php
+++ app/Admin/Controllers/GoodsController.php
@@ -28,7 +28,7 @@ class GoodsController extends Controller
     public function grid()
     {
         return Admin::grid(Good::class, function (Grid $grid) {
-            dd($grid->id());
+            $grid->id();
         });
     }
 }
