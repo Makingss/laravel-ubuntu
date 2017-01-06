ccf9578638e8bf2260b860bd604c23ba699acc71
making <wuyanping1029@foxmail.com>
2017-01-06 22:19:17 +0800

    GoodsController

--- app/Admin/Controllers/GoodsController.php
+++ app/Admin/Controllers/GoodsController.php
@@ -8,7 +8,9 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Model\Good;
 use Encore\Admin\Facades\Admin;
+use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 
 class GoodsController extends Controller
@@ -19,7 +21,14 @@ class GoodsController extends Controller
         return Admin::content(function (Content $content) {
             $content->header('商品信息');
             $content->description('商品信息列表');
-            $content->body('fdfafdsa');
+            $content->body($this->grid());
+        });
+    }
+
+    public function grid()
+    {
+        Admin::grid(Good::class, function (Grid $grid) {
+            dd($grid);
         });
     }
 }
---
+++ app/Model/Good.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Good extends Model
+{
+    protected $fillable = ['jooge_goods_id', 'bn', 'name', 'type_id', 'cat_id', 'brand_id', 'marketable', 'store', 'fav',
+        'notify_num', 'uptime', 'downtime', 'p_order', 'p_vstore', 'd_order', 'd_vstore', 'score', 'cost_price', 'mkt_price',
+        'price', 'member_price', 'activity_price', 'weight', 'g_weight', 'unit', 'brief', 'goods_type', 'image_default_id',
+        'udfimg', 'thumbnail_pic', 'small_pic', 'big_pic', 'pic_id', 'intro', 'store_place', 'min_buy', 'package_scale', 'package_unit',
+        'package_use', 'store_prompt', 'nostore_sell', 'goods_setting', 'spec_desc', 'params', 'visit_count', 'comments_count',
+        'view_w_count', 'view_count', 'count_stat', 'buy_count', 'buy_w_count', 'barcode', 'is_line', 'fx_1_price', 'fx_2_price',
+        'fx_3_price', 'goods_status', 'modify_status', 'price_modify', 'good_form', 'buy_limit', 'taxrate', 'tip_id', 'pmt_tag',
+        'pmt_id', 'goods_profit_ratio', 'is_pkg', 'pkg_info'
+    ];
+
+    public function goods_types()
+    {
+        $this->belongsTo(Goods_type::class, 'type_id');
+    }
+
+    public function products(){
+        $this->hasMany(Product::class,'goods_id');
+    }
+}
