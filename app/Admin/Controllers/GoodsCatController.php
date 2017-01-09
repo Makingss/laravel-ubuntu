29be2c43c781282f5903c3a03ef96e956e55a228
making <wuyanping1029@foxmail.com>
2017-01-09 20:58:01 +0800

    GoodsCatController

--- app/Admin/Controllers/GoodsCatController.php
+++ app/Admin/Controllers/GoodsCatController.php
@@ -75,7 +75,6 @@ class GoodsCatController extends Controller
             $form->display('cat_id','id');
             $form->select('type_id', '类型')->options(function () {
                 $goods_types = Goods_type::all();
-                dd($goods_types);
                 foreach ($goods_types as $goods_type) {
                     $type_names = array_add([], $goods_type->cat_id, $goods_type->name);
                 }
