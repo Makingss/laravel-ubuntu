067cedc758b15ae83e5bb58371df3a660288db04
making <wuyanping1029@foxmail.com>
2017-01-09 14:26:53 +0800

    goods_cat

--- app/Model/Goods_cat.php
+++ app/Model/Goods_cat.php
@@ -6,11 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Goods_cat extends Model
 {
-    protected $primarykey = 'cat_id';
+    protected $primaryKey = 'cat_id';
     protected $fillable = ['parent_id', 'type_id', 'name', 'is_leaf', 'gallery_setting', 'disabled', 'p_order', 'goods_count', 'cat_path'];
 
-//    public function Goods_types()
-//    {
-//        return $this->belongsTo(Goods_type::class, 'type_id');
-//    }
+    public function Goods_types()
+    {
+        return $this->belongsTo(Goods_type::class, 'type_id');
+    }
 }
