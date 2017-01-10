6be8527181d1073c89dd0f663d52a7305d36bfed
making <wuyanping1029@foxmail.com>
2017-01-10 16:57:32 +0800

    goods_cat

--- app/Model/Goods_cat.php
+++ app/Model/Goods_cat.php
@@ -3,9 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\EloquentSortable\Sortable;
+use Spatie\EloquentSortable\SortableTrait;
 
-class Goods_cat extends Model
+class Goods_cat extends Model implements Sortable
 {
+    use SortableTrait;
     protected $primaryKey = 'cat_id';
     protected $fillable = ['parent_id', 'type_id', 'name', 'is_leaf', 'gallery_setting', 'disabled', 'p_order', 'goods_count', 'cat_path'];
 
