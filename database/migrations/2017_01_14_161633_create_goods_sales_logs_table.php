ac23de3284afed980df33789839684757526c947
making <wuyanping1029@foxmail.com>
2017-01-14 17:16:58 +0800

    goods_sales_logs

--- database/migrations/2017_01_14_161633_create_goods_sales_logs_table.php
+++ database/migrations/2017_01_14_161633_create_goods_sales_logs_table.php
@@ -14,17 +14,17 @@ class CreateGoodsSalesLogsTable extends Migration
     public function up()
     {
         Schema::create(config('dbschema.databases.goods_sales_logs_tables'), function (Blueprint $table) {
-            $table->engine='InnoDB';
+            $table->engine = 'InnoDB';
             $table->increments('log_id');
             $table->integer('member_id')->comment('会员ID');
             $table->unsignedBigInteger('order_id')->comment('订单号');
-            $table->string('name',50)->nullable()->comment('会员名称');
-            $table->decimal('price',11,3)->nullable()->comment('价格');
+            $table->string('name', 50)->nullable()->comment('会员名称');
+            $table->decimal('price', 11, 3)->nullable()->comment('价格');
             $table->integer('product_id')->unsigned()->comment('产品ID');
             $table->integer('goods_id')->unsigned()->comment('商品ID');
-            $table->string('product_name',255)->nullable()->comment('产品名称');
-            $table->string('spec_info',255)->nullable()->comment('商品规格');
-            $table->integer('number',8)->nullable()->comment('购买数量');
+            $table->string('product_name', 255)->nullable()->comment('产品名称');
+            $table->string('spec_info', 255)->nullable()->comment('商品规格');
+            $table->integer('number')->nullable()->comment('购买数量');
             $table->timestamps();
         });
     }
@@ -38,4 +38,5 @@ class CreateGoodsSalesLogsTable extends Migration
     {
         Schema::dropIfExists(config('dbschema.databases.goods_sales_logs_tables'));
     }
+
 }
