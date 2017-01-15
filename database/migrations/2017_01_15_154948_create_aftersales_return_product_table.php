7b151658be6b58680aecbb16c6e3ca4f23efd1e7
trymoo <trymoo_zhang@163.com>
2017-01-15 16:10:55 +0800

    dbschame

---
+++ database/migrations/2017_01_15_154948_create_aftersales_return_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAftersalesReturnProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('dbschema.databases.aftersales_return_products_tables'), function (Blueprint $table) {
+            $table->increments('return_id');
+            $table->bigInteger('order_id')->nullable()->comment('订单号');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('dbschema.databases.aftersales_return_products_tables');
+    }
+}
