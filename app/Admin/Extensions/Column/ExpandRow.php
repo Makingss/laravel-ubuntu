92a2820c8e04fe71d25a32f0f9f1bdaabae1c0ac
makingss <wuyanping1029@foxmail.com>
2017-02-15 15:29:25 +0800

    delete vndor

---
+++ app/Admin/Extensions/Column/ExpandRow.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Making
+ * Date: 2017/1/20
+ * Time: 14:36
+ */
+namespace App\Admin\Extensions;
+
+use Encore\Admin\Grid\Displayers\AbstractDisplayer;
+
+class ExpandRow extends AbstractDisplayer
+{
+    public function display()
+    {
+        // TODO: Implement display() method.
+        return  "取不到这个方法！";
+    }
+}
--- app/Admin/Extensions/ExpandRow.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Making
- * Date: 2017/1/20
- * Time: 14:36
- */
